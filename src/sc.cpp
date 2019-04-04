aeb0ecd0b457da406b7e9b494a394544d2c95240
darshan <dshah2012@gmail.com>
2019-04-04 01:05:21 -0400

    intermediate progress

--- src/sc.cpp
+++ src/sc.cpp
@@ -10,8 +10,6 @@ using namespace std;
 
 bool seam_carving(Mat& in_image, int new_width, int new_height, Mat& out_image){
 
-    // some sanity checks
-    // Check 1 -> new_width <= in_image.cols
     if(new_width>in_image.cols){
         cout<<"Invalid request!!! new_width has to be smaller than the current size!"<<endl;
         return false;
@@ -31,100 +29,105 @@ bool seam_carving(Mat& in_image, int new_width, int new_height, Mat& out_image){
         cout<<"Invalid request!!! new_height has to be positive!"<<endl;
         return false;
         
-    }
-
-    
+    } 
     return seam_carving_trivial(in_image, new_width, new_height, out_image);
 }
 
+void rotate_image(Mat& in_image, Mat& out_image) {
+    rotate(in_image, out_image, ROTATE_90_CLOCKWISE);
+    out_image.copyTo(in_image);
+	cout<<"Rotated"<<endl;
+}
 
-// seam carves by removing trivial seams
+void rotate_image_anticlockwise(Mat& in_image, Mat& out_image) {
+    rotate(in_image, out_image, ROTATE_90_COUNTERCLOCKWISE);
+    out_image.copyTo(in_image);
+}
+
 bool seam_carving_trivial(Mat& in_image, int new_width, int new_height, Mat& out_image){
 
     Mat iimage = in_image.clone();
     Mat oimage = in_image.clone();
- while(iimage.rows!=new_height || iimage.cols!=new_width){
-        // horizontal seam if needed
-        if(iimage.rows>new_height){
+ while(iimage.cols!=new_width){
+      /*  if(iimage.rows>new_height){
             reduce_horizontal_seam_trivial(iimage, oimage);
             iimage = oimage.clone();
-        }
-        
+        }*/
         if(iimage.cols>new_width){
+			cout<<iimage.cols<<endl;
+            reduce_vertical_seam_trivial(iimage, oimage);
+            iimage = oimage.clone();
+        }
+ }
+ rotate_image(iimage, oimage);
+ while(iimage.cols!=new_height){
+		if(iimage.cols>new_height){
+			cout<<iimage.cols<<endl;
             reduce_vertical_seam_trivial(iimage, oimage);
             iimage = oimage.clone();
         }
     }
-    
-    
+	rotate_image_anticlockwise(iimage, oimage);
     out_image = oimage.clone();
     return true;
 }
 
-// horizontl trivial seam is a seam through the center of the image
-bool reduce_horizontal_seam_trivial(Mat& in_image, Mat& out_image){
+Mat getEnergy(Mat &in_image){
+	Mat src, src_gray;
+  	Mat energy;
+	int scale = 1;
+  	int delta = 0;
+    int ddepth = CV_16S;
+	src = in_image.clone();
+   	GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
+    cvtColor( src, src_gray, CV_BGR2GRAY );
+	Mat grad_x, grad_y;
+  	Mat abs_grad_x, abs_grad_y;
+    Sobel( src_gray, grad_x, ddepth, 1, 0,3, scale, delta, BORDER_DEFAULT );
+    Sobel( src_gray, grad_y, ddepth, 0, 1,3,scale, delta, BORDER_DEFAULT );
+	convertScaleAbs( grad_x, abs_grad_x );
+	convertScaleAbs( grad_y, abs_grad_y );
+	addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, energy);
+	return energy;
+}
 
-    // retrieve the dimensions of the new image
+
+// horizontl trivial seam is a seam through the center of the image
+/*bool reduce_horizontal_seam_trivial(Mat& in_image, Mat& out_image){
+
     int rows = in_image.rows;
     int cols = in_image.cols;
     
     // create an image slighly smaller
     out_image = Mat(rows-1, cols, CV_8UC3);
-//calculating energy of image 
-   Mat src, src_gray;
-  	Mat energy,energyi;
-	int scale = 1;
-  	int delta = 0;
-         int ddepth = CV_16S;
-	src = in_image.clone();
-   	GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
-
-        cvtColor( src, src_gray, CV_BGR2GRAY );
-	Mat grad_x, grad_y;
-  	Mat abs_grad_x, abs_grad_y;
-	
-         Sobel( src_gray, grad_x, ddepth, 1, 0,3, scale, delta, BORDER_DEFAULT );
-         Sobel( src_gray, grad_y, ddepth, 0, 1,3,scale, delta, BORDER_DEFAULT );
-	convertScaleAbs( grad_x, abs_grad_x );
-	convertScaleAbs( grad_y, abs_grad_y );
-	addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, energy);
-
-
-
-int tt=0;
-int tt1=0;
-
-        
-        int val[rows][cols];
-    
-          
-        for(int i=0;i<=rows-1;++i)
-
-        {
-              val[i][cols-1]=abs((double)energy.at<char>(i,cols-1));
-
-        }
-
+	//calculating energy of image 
+	Mat energy=getEnergy(in_image);
+	int tt=0;	
+	int tt1=0;
+    int val[rows][cols];
+    for(int i=0;i<=rows-1;++i)
+     {
+        val[i][cols-1]=abs((double)energy.at<char>(i,cols-1));
+     }
         for(int i=cols-2;i>=0;--i)
         {
 
             for(int j=0;j<=rows-1;++j)
             {
               if(j==0)
-                  {val[j][i]=min(abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j,i+1))+val[j][i+1],abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j+1,i+1))+val[j+1][i+1]);
+                  {
+					  val[j][i]=min(abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j,i+1))+val[j][i+1],abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j+1,i+1))+val[j+1][i+1]);
                   }
 
                  else if(j==rows-1)
-                     {val[j][i]=min(abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j,i+1))+val[j][i+1],abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j-1,i+1))+val[j-1][i+1]);
+                     {
+						 val[j][i]=min(abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j,i+1))+val[j][i+1],abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j-1,i+1))+val[j-1][i+1]);
                      } 
                    else
-                       { val[j][i]=min(abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j,i+1))+val[j][i+1],min(abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j+1,i+1))+val[j+1][i+1],abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j-1,i+1))+val[j-1][i+1]));
-                         
-                        }
-                          
-
+                       { 
+				   val[j][i]=min(abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j,i+1))+val[j][i+1],min(abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j+1,i+1))+val[j+1][i+1],abs((double)energy.at<char>(j,i)-(double)energy.at<char>(j-1,i+1))+val[j-1][i+1]));
+                       }
             }
-
          }
 
 int temp2;
@@ -217,7 +220,7 @@ s.pop();
 
     return true;
 }
-
+*/
 
 bool reduce_vertical_seam_trivial(Mat& in_image, Mat& out_image){
     
@@ -226,25 +229,8 @@ bool reduce_vertical_seam_trivial(Mat& in_image, Mat& out_image){
     
    
     out_image = Mat(rows, cols-1, CV_8UC3);
-    
-   Mat src, src_gray;
-  	Mat energy,energyi;
-	int scale = 1;
-  	int delta = 0;
-         int ddepth = CV_16S;
-	src = in_image.clone();
-   	GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
-       
-         cvtColor( src, src_gray, CV_BGR2GRAY );
-	Mat grad_x, grad_y;
-  	Mat abs_grad_x, abs_grad_y;
-        
-          Sobel( src_gray, grad_x, ddepth, 1, 0,3, scale, delta, BORDER_DEFAULT );
-         Sobel( src_gray, grad_y, ddepth, 0, 1,3,scale, delta, BORDER_DEFAULT );
-
-	convertScaleAbs( grad_x, abs_grad_x );
-	convertScaleAbs( grad_y, abs_grad_y );
-	addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, energy );
+    Mat energy=getEnergy(in_image);
+ 
       
      
     
@@ -336,16 +322,12 @@ for(int i=rows-1;i>0;--i)
                }
 
 }
-
-
-
-
 for (int i = 0; i< rows; ++i)
 {
 	if (s.top()<cols-1)
-                        {
+        {
 		for (int j = 0;j<s.top();++j)
-                                           {
+           {
                        Vec3b pixel = in_image.at<Vec3b>(i, j);
             
 
